941d755c034527cd513c761c2f11f0132111de1a
Travis Valentine <travis@yourturn.org>
2015-10-11 22:22:13 -0400

    Lock down embed format and remove css

--- app/controllers/embeds_controller.rb
+++ app/controllers/embeds_controller.rb
@@ -4,7 +4,13 @@ class EmbedsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :show
 
   def show
-    @widget = Widget.find(params[:id])
-    render layout: false
+    # Since this should only be called with JS that's the only
+    # format we'll support
+    respond_to do |format|
+      format.js do
+        @widget = Widget.find(params[:id])
+        render layout: false
+      end
+    end
   end
 end
